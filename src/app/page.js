0c78fbc217baf0368da3a6cb9c6b74482817a998
srs-sieam <srssieam@gmail.com>
2023-12-01 21:27:16 +0600

    convert server component into client component

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
-import React from 'react';
+"use client";
+import React, { useState } from 'react';
 
 const HomePage = () => {
+  const [counter, setCounter] = useState(0);
   return (
     <div>
       <h1>This is next app Home page</h1>
+      <h2>Counter: {counter}</h2>
+      <button className='px-3 py-2 bg-green-600 text-black mr-3' onClick={() => setCounter(counter +1)}>Increase</button>
+      <button className='px-3 py-2 bg-red-500 text-black' onClick={() => setCounter(counter -1)}>Decrease</button>
     </div>
   );
 };
