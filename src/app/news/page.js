f42b9332007ba379ca91e2de23e506bdc5104385
srs-sieam <srssieam@gmail.com>
2023-12-01 12:21:53 +0600

    nested routing

---
+++ src/app/news/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const newsHomePage = () => {
+    return (
+        <div>
+            <h1>This is news home page</h1>
+        </div>
+    );
+};
+
+export default newsHomePage;
