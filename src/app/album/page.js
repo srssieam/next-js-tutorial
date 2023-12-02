256636113bb210dfe35579e7602e4641792acbde
srs-sieam <srssieam@gmail.com>
2023-12-02 08:21:04 +0600

    import alias: optimization image from local folder

--- src/app/album/page.js
+++ src/app/album/page.js
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-
+import HouseImg from "@/assets/Lakeside Cottage.jpg"
 
 const AlbumPage = () => {
     return (
@@ -7,6 +7,13 @@ const AlbumPage = () => {
             <h1>Using Image component:</h1>
             <Image 
             src="https://i.ibb.co/2dPX5n8/Chicken-Tikka-Masala.jpg"
+            alt='food image'
+            width={500}
+            height={250} />
+
+            <h1 className='mt-8'>Using Image component: (from assets folder)</h1>
+            <Image 
+            src={HouseImg}
             alt='food image'
             width={500}
             height={250} />
