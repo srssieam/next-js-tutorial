604194378af76745f8baa68c38994ede179c229f
srs-sieam <srssieam@gmail.com>
2023-12-02 06:44:28 +0600

    created error page

---
+++ src/app/not-found.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div className='h-[100vh] w-full flex justify-center items-center'>
+            <h1 className='text-red-700 text-6xl font bold'>404 !!! page not found</h1>
+        </div>
+    );
+};
+
+export default NotFound;
