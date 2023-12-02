2371340e62cae0de8ce1b4179fb53527545141c5
srs-sieam <srssieam@gmail.com>
2023-12-02 06:57:55 +0600

    created header and footer

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Counter from "@/components/Counter/Counter";
 
 
 const HomePage = () => {
-  throw new Error();
+  // throw new Error();
   return (
     <div>
       <h1>This is next app Home page</h1>
