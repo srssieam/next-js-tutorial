c1ce6b689aed4e8d2f5b1821299d3aa73593d329
srs-sieam <srssieam@gmail.com>
2023-12-02 07:18:36 +0600

    navigation with Link component

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Counter from "@/components/Counter/Counter";
+import Link from "next/link";
 
 
 const HomePage = () => {
@@ -7,6 +8,12 @@ const HomePage = () => {
     <div>
       <h1>This is next app Home page</h1>
       <Counter></Counter>
+      <Link href="/about">
+        <button className="text-green-500 underline font-bold mt-5">Go to About page</button>
+      </Link>
+      <Link href="/news">
+        <button className="text-green-500 ml-5 underline font-bold mt-5">Go to News page</button>
+      </Link>
     </div>
   );
 };
